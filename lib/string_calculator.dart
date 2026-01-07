b7c405ec93a7ad97c51c089018eee80426e45297
rahulsharma <rahulsharma.geekologix@gmail.com>
2026-01-07 20:43:30 +0530

    GREEN: support single character custom delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,9 +2,19 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final normalized = numbers.replaceAll('\n', ',');
+    String delimiter = ',';
+    String body = numbers;
+
+    if (numbers.startsWith('//')) {
+      final parts = numbers.split('\n');
+      delimiter = parts[0].substring(2);
+      body = parts[1];
+    }
+
+    final normalized = body.replaceAll('\n', delimiter);
+
     return normalized
-        .split(',')
+        .split(delimiter)
         .map(int.parse)
         .fold(0, (sum, n) => sum + n);
   }
